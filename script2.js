07e74147592580830ad945807f01a1b8b2630409
Daniel3405 <d.vilchez0304@gmail.com>
2025-05-26 14:09:38 -0400

    inicio1

--- script2.js
+++ script2.js
@@ -1,2 +1,31 @@
 //validar nombre, debe contener solo letras y no ser vacio
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
+
+Registro = []
+
+function validar(){
+    let eNombre = document.getElementById("nombre")
+    let vNombre = eNombre.value()
+    let errorNombre = document.getElementById("Error Nombre")
+
+    let eEdad = document.getElementById("edad")
+    let vEdad = eEdad.value()
+    let errorEdad = document.getElementById("Error edad")
+}
+
+function validarcarateres(){
+    if(elemento,valor,error){
+        console.log("Solo letras")
+        alert("Debes ingresar solo letras")
+        eError.innerText = "Debes ingresar solo letras!"
+        
+        
+        return "falla"
+    }else{
+        console.log("Todo bien")
+        eError.innerText = ""
+        return "exito"
+    }
+}
+
+
